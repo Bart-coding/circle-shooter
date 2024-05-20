32854eeb1318dff798df621160a66a87d084d2f3
Bart-coding <bartekpoczta54@gmail.com>
2024-05-21 01:47:33 +0200

    Add sound effects

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # circle-shooter
-<h2> A colorful shooting game - having fun with p5.js </h2>
-For now, the game allows you to simply shoot a small, independently moving circle at a large, minimally orbiting circle. The fired circles are of a random color and color the large circle, which increases the hit counter.
-You can also activate the mode of clearing the board of fired circles. <br> <br>
-
+### A colorful shooting game - having fun with p5.js
 ![obraz](https://github.com/Bart-coding/circle-shooter/assets/74357483/38e1a081-785e-4916-859c-f299af497270)
+For now, the game allows you to simply shoot a small, independently moving circle at a large, minimally orbiting circle. The fired circles are of a random color and color the large circle, which increases the hit counter.
+You can also:
+* activate the clean mode to clear the board of fired circles
+* change volume of sound effects: left/right arrow key to volume down/up and "M" key to mute/unmute volume.
+To play the game, you will probably have to use [a local server](https://github.com/processing/p5.js/wiki/Local-server).
+Kudos are due to the creators of the sound effects who posted them on Pixabay.
--- scripts/circleShooter.js
+++ scripts/circleShooter.js
@@ -4,8 +4,19 @@ let button, buttonX, buttonY, buttonHelperFactor;
 let shottedCircles = [];
 let hitsCounter;
 let bigCircleColor;
-let cleaningMode = false;
-let lastShotFrameCount = -15;
+let cleaningMode;
+let lastShotFrameCount;
+let shotSound, hitSound, currentVolume, mute, muteVarChangedFramesCount, framesToViewMuteInfo;
+
+function preload() {
+  shotSound = loadSound("assets/blaster-2-81267.mp3");
+  hitSound = loadSound("assets/power-up-sparkle-1-177983.mp3");
+  mute = false;
+  framesToViewMuteInfo = 20;
+  currentVolume = 0.02;
+  shotSound.setVolume(currentVolume);
+  hitSound.setVolume(currentVolume);
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -25,11 +36,15 @@ function setup() {
   
   shottedCircleRadius = 15;
   hitsCounter = 0;
+  lastShotFrameCount = -15;
   
   button = createButton('SWITCH');
   buttonX = 580;
   buttonY = 45;
   button.position(buttonX, buttonY);
+  
+  cleaningMode = false;
+  monoSynth = new p5.MonoSynth();
   button.mousePressed(() => {
     cleaningMode = !cleaningMode;
   });
@@ -58,6 +73,8 @@ function drawShottedCircles() {
       circleColor: circle_Color,
       shotted: Date.now()
     });
+
+    if (!mute) shotSound.play();
   }
 
   for (let i = shottedCircles.length - 1; i >= 0; --i) {
@@ -81,6 +98,8 @@ function drawShottedCircles() {
         button.position((buttonX = buttonX + 25), buttonY);
         buttonHelperFactor *= 10;
       }
+
+	  if (!mute) hitSound.play();
     }
   }
 }
@@ -121,9 +140,64 @@ function drawSmallCircle() {
   circle(smallCircleX, smallCircleY, smallCircleRadius * 2);
 }
 
+function drawVolumeInfoText(message) {
+  textSize(40);
+  fill(255, 200, 0);
+  text(message, 55, 120);
+}
+
+function modifyVolume() {
+  if (keyIsDown(LEFT_ARROW)) {
+    currentVolume -= 0.0005;
+    if (currentVolume <= 0) {
+      currentVolume = 0;
+      mute = true;
+      muteVarChangedFramesCount = framesToViewMuteInfo;
+    }
+    shotSound.setVolume(currentVolume);
+    hitSound.setVolume(currentVolume);
+    drawVolumeInfoText("volume down");
+  }
+
+  else if (keyIsDown(RIGHT_ARROW)) {
+    if (mute === true) {
+      mute = false;
+      muteVarChangedFramesCount = framesToViewMuteInfo;
+    }
+    if (currentVolume <= 0.5) {
+      currentVolume += 0.0005;
+      shotSound.setVolume(currentVolume);
+      hitSound.setVolume(currentVolume);
+      drawVolumeInfoText("volume up");
+    }
+  }
+
+  if (muteVarChangedFramesCount > 0) {
+    --muteVarChangedFramesCount;
+    if (mute) drawVolumeInfoText("volume muted");
+    else drawVolumeInfoText("volume unmuted");
+  }
+}
+
 function draw() {
   drawHeader();
   drawShottedCircles();
   drawBigCircle();
   drawSmallCircle();
+  modifyVolume();
+}
+
+function keyPressed() {
+  if (key === "m" || key === "M") {
+    if (mute === false) {
+      mute = true;
+      muteVarChangedFramesCount = framesToViewMuteInfo;
+      shotSound.stop();
+      hitSound.stop();
+    }
+    else {
+      mute = false;
+      muteVarChangedFramesCount = framesToViewMuteInfo;
+    }
+  }
 }
